c59557653e7cdbea8fa8699fd31fea99bdca4004
Antimen777 <167418909+Antimen777@users.noreply.github.com>
2024-07-02 12:14:36 +0300

    Update aggregate_last_paid_click.sql

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -1,15 +1,37 @@
-with tab as (
-    select
-        s.visit_date::date,
-        s.source,
+with last_click_sess as (
+	select
+		visitor_id,
+		max(visit_date) as last_visit,
+		count(visitor_id) as visit_count
+	from sessions
+	where medium != 'organic'
+	group by 1
+),
+
+without_double as (
+	select
+		distinct s.visitor_id,
+		s.source, 
         s.medium,
         s.campaign,
-        count(s.visitor_id) as visitors_count,
+        lc.last_visit,
+        lc.visit_count
+	from sessions s 
+	inner join last_click_sess lc on s.visitor_id = lc.visitor_id and s.visit_date = lc.last_visit
+),
+
+tab as (
+    select
+        wd.last_visit::date,
+        wd.source,
+        wd.medium,
+        wd.campaign,
+        sum(wd.visit_count) as visitors_count,
         count(l.lead_id) as leads_count,
-        count(case when status_id = 142 then 1 end) as purchases_count,
+        count(case when l.amount > 0 then 1 end) as purchases_count,
         sum(l.amount) as revenue
-    from sessions as s
-    left join leads as l on s.visitor_id = l.visitor_id
+    from without_double as wd
+    left join leads as l on wd.visitor_id = l.visitor_id
     group by 1, 2, 3, 4
 ),
 
@@ -34,20 +56,20 @@ ads as (
 )
 
 select
-    t.visit_date,
-    a.utm_source,
-    a.utm_medium,
-    a.utm_campaign,
+    t.last_visit as visit_date,
+    t.source as utm_source,
+    t.medium as utm_medium,
+    t.campaign as utm_campaign,
     t.visitors_count,
     a.total_cost,
     t.leads_count,
     t.purchases_count,
     t.revenue
 from tab as t
-inner join
+left join
     ads as a
     on
-        t.visit_date = a.campaign_date
+        t.last_visit = a.campaign_date
         and t.source = a.utm_source
         and t.medium = a.utm_medium
         and t.campaign = a.utm_campaign
