e7e0a8b9247a9c8633cdea39657563ab0f6a7172
Antimen777 <167418909+Antimen777@users.noreply.github.com>
2024-07-08 11:04:16 +0300

    Update aggregate_last_paid_click.sql

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -1,23 +1,34 @@
 with last_click_sess as (
-	select
-		visitor_id,
-		max(visit_date) as last_visit,
-		count(visitor_id) as visit_count
-	from sessions
-	where medium != 'organic'
-	group by 1
+    select
+        visitor_id,
+        max(visit_date) as last_visit
+    from sessions
+    where medium != 'organic'
+    group by 1
 ),
 
 without_double as (
-	select
-		distinct s.visitor_id,
-		s.source, 
+    select distinct
+        s.visitor_id,
+        s.source,
         s.medium,
         s.campaign,
-        lc.last_visit,
-        lc.visit_count
-	from sessions s 
-	inner join last_click_sess lc on s.visitor_id = lc.visitor_id and s.visit_date = lc.last_visit
+        lc.last_visit
+    from sessions as s
+    inner join
+        last_click_sess as lc
+        on s.visitor_id = lc.visitor_id and s.visit_date = lc.last_visit
+),
+
+visitors as (
+    select
+        last_visit::date,
+        source,
+        medium,
+        campaign,
+        count(visitor_id) as visitors_count
+    from without_double
+    group by 1, 2, 3, 4
 ),
 
 tab as (
@@ -26,12 +37,13 @@ tab as (
         wd.source,
         wd.medium,
         wd.campaign,
-        sum(wd.visit_count) as visitors_count,
         count(l.lead_id) as leads_count,
         count(case when l.amount > 0 then 1 end) as purchases_count,
         sum(l.amount) as revenue
     from without_double as wd
-    left join leads as l on wd.visitor_id = l.visitor_id
+    left join
+        leads as l
+        on wd.visitor_id = l.visitor_id and wd.last_visit < l.created_at
     group by 1, 2, 3, 4
 ),
 
@@ -57,15 +69,22 @@ ads as (
 
 select
     t.last_visit as visit_date,
+    v.visitors_count,
     t.source as utm_source,
     t.medium as utm_medium,
     t.campaign as utm_campaign,
-    t.visitors_count,
     a.total_cost,
     t.leads_count,
     t.purchases_count,
     t.revenue
 from tab as t
+inner join
+    visitors as v
+    on
+        t.last_visit = v.last_visit
+        and t.source = v.source
+        and t.medium = v.medium
+        and t.campaign = v.campaign
 left join
     ads as a
     on
