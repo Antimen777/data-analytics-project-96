d1bb09db3c73e7e8bbfd49f8888010fc9e7b6b64
Antimen777 <167418909+Antimen777@users.noreply.github.com>
2024-07-08 12:06:46 +0300

    Update dashboard.sql

--- dashboard.sql
+++ dashboard.sql
@@ -1,32 +1,51 @@
 --Датасет для группировки по дате и источнику
 --Группировка по последнему клику
 with last_click_sess as (
-	select
-		visitor_id,
-		max(visit_date) as last_visit,
-		count(visitor_id) as visit_count
-	from sessions
-	group by 1
+    select
+        visitor_id,
+        max(visit_date) as last_visit
+    from sessions
+    where medium != 'organic'
+    group by 1
 ),
+
 --Убирание дубликатов, так как есть полные дубли в sessions
 without_double as (
-	select distinct s.visitor_id, s.source, lc.last_visit, lc.visit_count
-	from sessions s 
-	join last_click_sess lc on s.visitor_id = lc.visitor_id and s.visit_date = lc.last_visit
+    select distinct
+        s.visitor_id,
+        s.source,
+        lc.last_visit
+    from sessions as s
+    inner join
+        last_click_sess as lc
+        on s.visitor_id = lc.visitor_id and s.visit_date = lc.last_visit
 ),
---Агрегация
+
+--Агрегация отдельно уникальных посетителей
+visitors as (
+    select
+        last_visit::date,
+        source,
+        count(visitor_id) as visitors_count
+    from without_double
+    group by 1, 2
+),
+
+--Агрегация всего остального с учётом логики: создание лида позже посещения
 tab as (
     select
         wd.last_visit::date,
         wd.source,
-        sum(wd.visit_count) as visitors_count,
         count(l.lead_id) as leads_count,
         count(case when l.amount > 0 then 1 end) as purchases_count,
         sum(l.amount) as revenue
     from without_double as wd
-    left join leads as l on wd.visitor_id = l.visitor_id
+    left join
+        leads as l
+        on wd.visitor_id = l.visitor_id and wd.last_visit < l.created_at
     group by 1, 2
 ),
+
 --Соединение нужных данных по рекламе
 ads as (
     select
@@ -43,17 +62,25 @@ ads as (
     from ya_ads
     group by 1, 2
 )
+
 --Итоговая таблица
 select
-    coalesce(t.last_visit, a.campaign_date) as calculation_date,
-    a.utm_source,
-    t.visitors_count,
+    t.last_visit as visit_date,
+    v.visitors_count,
+    t.source as utm_source,
+    t.medium as utm_medium,
+    t.campaign as utm_campaign,
+    a.total_cost,
     t.leads_count,
     t.purchases_count,
-    t.revenue,
-    a.total_cost
+    t.revenue
 from tab as t
-right join
+inner join
+    visitors as v
+    on
+        t.last_visit = v.last_visit
+        and t.source = v.source
+left join
     ads as a
     on
         t.last_visit = a.campaign_date
@@ -63,24 +90,36 @@ right join
 
 --Тоже самое только добавляется группировка по medium и campaign
 with last_click_sess as (
-	select
-		visitor_id,
-		max(visit_date) as last_visit,
-		count(visitor_id) as visit_count
-	from sessions
-	group by 1
+    select
+        visitor_id,
+        max(visit_date) as last_visit
+    from sessions
+    where medium != 'organic'
+    group by 1
 ),
 
 without_double as (
-	select
-		distinct s.visitor_id,
-		s.source, 
+    select distinct
+        s.visitor_id,
+        s.source,
         s.medium,
         s.campaign,
-        lc.last_visit,
-        lc.visit_count
-	from sessions s 
-	inner join last_click_sess lc on s.visitor_id = lc.visitor_id and s.visit_date = lc.last_visit
+        lc.last_visit
+    from sessions as s
+    inner join
+        last_click_sess as lc
+        on s.visitor_id = lc.visitor_id and s.visit_date = lc.last_visit
+),
+
+visitors as (
+    select
+        last_visit::date,
+        source,
+        medium,
+        campaign,
+        count(visitor_id) as visitors_count
+    from without_double
+    group by 1, 2, 3, 4
 ),
 
 tab as (
@@ -89,12 +128,13 @@ tab as (
         wd.source,
         wd.medium,
         wd.campaign,
-        sum(wd.visit_count) as visitors_count,
         count(l.lead_id) as leads_count,
         count(case when l.amount > 0 then 1 end) as purchases_count,
         sum(l.amount) as revenue
     from without_double as wd
-    left join leads as l on wd.visitor_id = l.visitor_id
+    left join
+        leads as l
+        on wd.visitor_id = l.visitor_id and wd.last_visit < l.created_at
     group by 1, 2, 3, 4
 ),
 
@@ -119,17 +159,24 @@ ads as (
 )
 
 select
-    coalesce(t.last_visit, a.campaign_date) as calculation_date,
-    a.utm_source,
-    a.utm_medium,
-    a.utm_campaign,
-    t.visitors_count,
+    t.last_visit as visit_date,
+    v.visitors_count,
+    t.source as utm_source,
+    t.medium as utm_medium,
+    t.campaign as utm_campaign,
+    a.total_cost,
     t.leads_count,
     t.purchases_count,
-    t.revenue,
-    a.total_cost
+    t.revenue
 from tab as t
-right join
+inner join
+    visitors as v
+    on
+        t.last_visit = v.last_visit
+        and t.source = v.source
+        and t.medium = v.medium
+        and t.campaign = v.campaign
+left join
     ads as a
     on
         t.last_visit = a.campaign_date
@@ -167,26 +214,28 @@ COUNTD_IF([lead_id], [amount] > 0) / COUNTD([lead_id]) * 100
 
 --Датасет для расчёта закрытия 90% лидов
 with last_click_sess as (
-	select
-		visitor_id,
-		max(visit_date) as last_visit
-	from sessions
-	group by 1
+    select
+        visitor_id,
+        max(visit_date) as last_visit
+    from sessions
+    group by 1
 ),
 
 without_double as (
-	select
-		distinct s.visitor_id,
+    select distinct
+        s.visitor_id,
         lc.last_visit
-	from sessions s 
-	inner join last_click_sess lc on s.visitor_id = lc.visitor_id and s.visit_date = lc.last_visit
-),
+    from sessions as s
+    inner join
+        last_click_sess as lc
+        on s.visitor_id = lc.visitor_id and s.visit_date = lc.last_visit
+)
 
 select
-	wd.visitor_id,
-	wd.last_visit,
-	l.created_at,
-	ntile(10) over(order by l.created_at - wd.last_visit)
+    wd.visitor_id,
+    wd.last_visit,
+    l.created_at,
+    ntile(10) over (order by l.created_at - wd.last_visit)
 from without_double as wd
 inner join leads as l on wd.visitor_id = l.visitor_id
 where l.created_at > wd.last_visit
